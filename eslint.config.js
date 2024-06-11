40fbd983db97806096df34e1831b4028eff54107
KimGreenbush <kimgreenbush.com>
2024-06-10 23:28:01 -0700

    config setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginVue from "eslint-plugin-vue"
+import vueParser from "vue-eslint-parser"
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default [
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	...pluginVue.configs["flat/recommended"],
+    {
+        languageOptions: {
+            globals: globals.browser,
+            ecmaVersion: "latest"
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs["flat/recommended"],
+    {
+        ignores: ["**/temp.js", "config/*"]
+    },
+    {
+        files: ["*.vue", "**/*.vue"],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: { parser: tseslint.parser, sourceType: "module" }
+        }
+    },
+    eslintConfigPrettier
 ]
